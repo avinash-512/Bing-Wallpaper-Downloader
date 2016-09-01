67c3b8eb67a00329cf6c30da75bcc88ea456928d
Avinash Venkatesh <abhinayavinash@iitkgp.ac.in>
2016-09-01 19:56:37 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 U=$USER
-su
-read USE
 sudo apt-get install python3
+sudo apt-get install python-setuptools python-dev build-essential
+sudo easy_install pip
 sudo pip install bs4
 sudo pip install crontab-python
 sudo pip install urllib
 sudo mkdir /home/$U/Bing_Wallpaper2.0
 sudo cp $PWD/Wallpaper_Ubuntu.py /home/$U/Bing_Wallpaper2.0/ -r
-SCRIPT_PATH=/home/$U/Scripts/Wallpaper_Ubuntu.py
+SCRIPT_PATH=/home/$U/Bing_Wallpaper2.0/Wallpaper_Ubuntu.py
 sudo /usr/bin/python3 $SCRIPT_PATH
 
 
